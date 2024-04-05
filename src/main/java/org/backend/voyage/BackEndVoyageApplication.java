83cc08c364c7f31f651660e6b2289295877306f3
Oussama-benrkia <oussamabenrkia0@gmail.com>
2024-04-05 21:58:04 +0000

    first commit

---
+++ src/main/java/org/backend/voyage/BackEndVoyageApplication.java
@@ -0,0 +1,13 @@
+package org.backend.voyage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndVoyageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndVoyageApplication.class, args);
+    }
+
+}
